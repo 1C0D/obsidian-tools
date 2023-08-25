3dd85f41559de0c6cdeeb541b7140143ef472437
1C0D <63018905+1C0D@users.noreply.github.com>
2023-08-26 00:07:12 +0200

    in progress

--- src/main.ts
+++ src/main.ts
@@ -1,91 +1,177 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+// npm i -D electron fs-extra 
+// npm i -D @types/fs-extra
+let dialog: any = null;
+try {
+	const electron = require("electron");
+	dialog = electron.remote.dialog;//  electron.dialog wrong
+	// dialog = electron.dialog;//  electron.dialog wrong
+} catch {
+	console.debug("electron not found");
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+import { App, Menu, MenuItem, Notice, Plugin, PluginSettingTab, TFile, normalizePath } from 'obsidian';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import { ConfirmModal } from './confirm_modal';
+
+//- move folder or file(s) out of the vault
+//- if a folder already exists and you accept to replace it, 
+//  it will move files and replace duplicate existing ones. 
+//- todo: plusieurs files
+
+interface ToolsSettings {
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: ToolsSettings = {
+}
+
+export default class Tools extends Plugin {
+	settings: ToolsSettings;
+	applyAll: boolean = false
+	files: TFile[]
 
 	async onload() {
 		await this.loadSettings();
+		this.addSettingTab(new ETVSettingTab(this.app, this));
+		this.registerEvent((this.app as any).
+			workspace.on("file-menu", (menu: Menu, file: TFile) => {
+				menu.addItem((item: MenuItem) => {
+					item.setTitle("Move Out From Vault");
+					item.setIcon("scissors");
+					item.onClick(async () => {
+						this.openFileExplorer(file)
+					});
+				})
+			}));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEvent((this.app as any).
+			workspace.on("files-menu", (menu: Menu, files: TFile[]) => {
+				menu.addItem((item: MenuItem) => {
+					item.setTitle("Move Out From Vault");
+					item.setIcon("scissors");
+					item.onClick(async () => {
+						this.openFileExplorer(files)
+					});
+				})
+			}))
 	}
 
-	onunload() {
+	openFileExplorer = async (file: TFile | TFile[]) => {
+		this.applyAll= false
+		try {
+			const selectedPath: string = await this.picker()
+			if (!selectedPath) return
+			this.treatFiles(file, selectedPath)
+		} catch (err) {
+			console.debug("err", err)
+			const notice = new Notice('Select a folder to copy the file to.');
+			notice.noticeEl.addClass('red-background') // document.querySelector('.notice')
+		}
+	};
 
+	treatFiles(file: TFile | TFile[], selectedPath: string) {
+
+		if (Array.isArray(file) && file.length) {
+			this.files = [...file]
+			this.MoveItem(this.files.pop() as TFile, selectedPath)
+		}
+		else {
+			this.MoveItem(file as TFile, selectedPath)
+		}
+	}
+
+	async MoveItem(file: TFile, selectedPath: string) {
+		if (!file) return
+		const { normalizedFullPath, fileName, destinationPath } = this.getDestinationPath(file as TFile, selectedPath)
+
+		await this.doCopy(normalizedFullPath, fileName, destinationPath, selectedPath)
+
+		// Remove the original file
+		// await fs.remove(normalizedFullPath);
+		console.debug('Original file has been deleted.');
+	}
+
+	async picker() {
+		let picker = await dialog.showOpenDialog({
+			properties: ['openDirectory'],
+		});
+
+		if (picker.canceled) {
+			console.debug('Folder selection was canceled.');
+			return;
+		}
+
+		return picker.filePaths[0];
+	}
+
+	getDestinationPath(file: TFile, selectedPath: string) {
+		const filePath = (this.app as any).vault.adapter.getFullPath(file.path);
+		const normalizedFullPath = normalizePath(filePath);
+		const fileName = path.basename(normalizedFullPath);
+		const destinationPath = path.join(selectedPath, fileName)
+		return { normalizedFullPath, fileName, destinationPath, };
+	}
+
+	async doCopy(
+		normalizedFullPath: string,
+		fileName: string,
+		destinationPath: string,
+		selectedPath: string,
+	) {
+		const fileExists = await fs.pathExists(destinationPath);
+		if (fileExists) { // file already exists
+			console.log("ici")
+
+			if (!this.applyAll) new ConfirmModal(this.app, this, (result) => {
+				if (result === 1) {
+					this.confirm(fileName, selectedPath, normalizedFullPath, destinationPath, 1)
+					if (this.files && this.files.length) {
+						this.MoveItem(this.files.pop() as TFile, selectedPath)
+					}
+				}
+				else if (result === 2) {
+					this.confirm(fileName, selectedPath, normalizedFullPath, destinationPath, 2)
+					if (this.files && this.files.length) {
+						this.MoveItem(this.files.pop() as TFile, selectedPath)
+					}
+				} else {
+					console.log("result", result)
+					return
+				}
+			}).open()
+		} else {
+			await fs.copy(normalizedFullPath, destinationPath);
+			console.debug('File was successfully copied.');
+		}
+	}
+
+	async confirm(
+		fileName: string,
+		selectedPath: string,
+		normalizedFullPath: string,
+		destinationPath: string,
+		choice:number
+	) {
+		if (choice === 2) {
+			// Create an incremented version
+			const baseFileName = path.parse(fileName).name;
+			const extension = path.parse(fileName).ext;
+			let version = 1;
+			let versionedFileName = `${baseFileName} (${version})${extension}`;
+			while (await fs.pathExists(path.join(selectedPath, versionedFileName))) {
+				version++;
+				versionedFileName = `${baseFileName} (${version})${extension}`;
+			}
+			await fs.copy(normalizedFullPath, path.join(selectedPath, versionedFileName));
+			console.debug('File was successfully copied as', versionedFileName);
+		} else {
+			await fs.copy(normalizedFullPath, destinationPath);
+			console.debug('File was successfully moved.');
+		}
 	}
 
 	async loadSettings() {
-		this.settings = {
-			...DEFAULT_SETTINGS, ...await this.loadData()
-		}		
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
@@ -93,42 +179,19 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class ETVSettingTab extends PluginSettingTab {
+	plugin: Tools;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	constructor(app: App, public plugin: MyPlugin) {
+	constructor(app: App, plugin: Tools) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
+		containerEl.createEl('h2', { text: 'Tools' });
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
