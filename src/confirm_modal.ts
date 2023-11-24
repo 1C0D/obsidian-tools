e6b9402ab76c98df2b2dd023a432e61c5968d47c
1C0D <1conscience@gmx.fr>
2023-11-24 23:29:12 +0100

    move out from vault[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C/copy from vault

--- src/confirm_modal.ts
+++ src/confirm_modal.ts
@@ -1,7 +1,7 @@
 import { App, Modal, Setting } from "obsidian";
 import Tools from "./main";
 
-export class ConfirmModal extends Modal {
+export class OutFromVaultConfirmModal extends Modal {
 	constructor(
 		app: App,
 		public plugin: Tools,
@@ -17,42 +17,46 @@ export class ConfirmModal extends Modal {
 		this.modalEl.style.height = `150px`;
 
 		const { contentEl } = this;
-		const eL = this.contentEl.createEl("p", {
-			text: `File/folder already exists`,
+		this.contentEl.createEl("p", {
+			text: `Some files already exist.`,
+		});
+		this.contentEl.createEl("p", {
+			text: `choose paste option:`,
 		});
 		// after newSetting checkbox applyAll
 		const newSetting = new Setting(this.contentEl);
-		const span = newSetting.settingEl.createEl("span", {
-			text: "apply to all",
-		});
-		span.style.marginLeft = "3px";
-		const checkbox = newSetting.settingEl.createEl("input", {
-			type: "checkbox",
-		});
-		checkbox.checked = this.plugin.applyAll;
-		checkbox.addEventListener("change", async () => {
-			this.plugin.applyAll = checkbox.checked;
-			await this.plugin.saveSettings();
-			console.log("this.plugin.applyAll After", this.plugin.applyAll);
-		});
+		// const span = newSetting.settingEl.createEl("span", {
+		// 	text: "apply to all",
+		// });
+		// span.style.marginLeft = "3px";
+		// const checkbox = newSetting.settingEl.createEl("input", {
+		// 	type: "checkbox",
+		// });
+		// checkbox.checked = this.plugin.applyAll;
+		// checkbox.addEventListener("change", async () => {
+		// 	this.plugin.applyAll = checkbox.checked;
+		// 	await this.plugin.saveSettings();
+		// 	console.log("this.plugin.applyAll After", this.plugin.applyAll);
+		// });
 
 		newSetting
 			.addButton((b) => {
-				b.setButtonText("replace")
+				b.setButtonText("Paste")
 					.setCta()
+					.setTooltip("overwrite existing files")
 					.onClick(async () => {
-						await this.plugin.saveSettings();
+						// await this.plugin.saveSettings();
 						this.close();
 						this.onSubmit(1);
 					});
 			})
 			.addButton((b) => {
-				b.setButtonText("incremental copy")
-					// .setIcon("copy-plus")
+				b.setButtonText("Incremental Paste")
+					.setIcon("copy-plus")
 					.setCta()
-					// .setTooltip("incremental copy")
+					.setTooltip("increment path if file exists")
 					.onClick(async () => {
-						await this.plugin.saveSettings();
+						// await this.plugin.saveSettings();
 						this.close();
 						this.onSubmit(2);
 					});
--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,16 @@ import {
 	Plugin,
 	PluginSettingTab,
 	TFile,
+	TFolder,
 	normalizePath,
 } from "obsidian";
 import * as fs from "fs-extra";
 import * as path from "path";
-import { ConfirmModal } from "./confirm_modal";
+import { OutFromVaultConfirmModal } from "./confirm_modal";
 
-//- move folder or file(s) out of the vault
+//- move folder(s) or file(s) out of the vault
 //- if a folder already exists and you accept to replace it,
 //  it will move files and replace duplicate existing ones.
-//- todo: plusieurs files
 
 declare global {
 	interface Window {
@@ -23,43 +23,59 @@ declare global {
 	}
 }
 
-interface ToolsSettings {}
+// interface ToolsSettings {}
 
-const DEFAULT_SETTINGS: ToolsSettings = {};
+// const DEFAULT_SETTINGS: ToolsSettings = {};
 
 export default class Tools extends Plugin {
-	settings: ToolsSettings;
-	applyAll: boolean = false;
-	files: TFile[];
+	// settings: ToolsSettings;
+	files: TFile[] | TFolder[];
 
 	async onload() {
-		await this.loadSettings();
-		this.addSettingTab(new ETVSettingTab(this.app, this));
+		// await this.loadSettings();
+		// this.addSettingTab(new ETVSettingTab(this.app, this));
+
+		// 1 file selection
 		this.registerEvent(
 			(this.app as any).workspace.on(
 				"file-menu",
-				(menu: Menu, file: TFile) => {
+				(menu: Menu, file: TFile | TFolder) => {
 					menu.addItem((item: MenuItem) => {
-						item.setTitle("Move Out From Vault");
-						item.setIcon("scissors");
+						item.setTitle("Copy Out From Vault");
+						item.setIcon("copy");
 						item.onClick(async () => {
 							this.openFileExplorer(file);
 						});
 					});
-				}
-			)
-		);
-
-		this.registerEvent(
-			(this.app as any).workspace.on(
-				"files-menu",
-				(menu: Menu, files: TFile[]) => {
 					menu.addItem((item: MenuItem) => {
 						item.setTitle("Move Out From Vault");
 						item.setIcon("scissors");
 						item.onClick(async () => {
-							console.log("files", files);
+							this.openFileExplorer(file, true);
+						});
+					});
+				}
+			)
+		);
+		// multi files selection (or directories?)
+		this.registerEvent(
+			(this.app as any).workspace.on(
+				"files-menu",
+				(menu: Menu, files: TFile[] | TFolder[]) => {
+					menu.addItem((item: MenuItem) => {
+						item.setTitle("Copy Out From Vault");
+						item.setIcon("copy");
+						item.onClick(async () => {
+							// console.log("files", files);
 							this.openFileExplorer(files);
+						});
+					});
+					menu.addItem((item: MenuItem) => {
+						item.setTitle("Move Out From Vault");
+						item.setIcon("scissors");
+						item.onClick(async () => {
+							// console.log("files", files);
+							this.openFileExplorer(files, true);
 						});
 					});
 				}
@@ -67,43 +83,125 @@ export default class Tools extends Plugin {
 		);
 	}
 
-	openFileExplorer = async (file: TFile | TFile[]) => {
-		this.applyAll = false;
-		try {
-			const selectedPath: string = await this.picker();
-			if (!selectedPath) return;
-			this.treatFiles(file, selectedPath);
-		} catch (err) {
-			console.debug("err", err);
-			const notice = new Notice("Select a folder to copy the file to.");
-			notice.noticeEl.addClass("red-background");
+	async doesFileExists(
+		file: TFile | TFolder,
+		selectedPath: string
+	): Promise<boolean> {
+		const { destinationPath } = this.getDestinationPath(
+			file as TFile | TFolder,
+			selectedPath
+		);
+		const fileExists = await fs.pathExists(destinationPath);
+		if (fileExists) {
+			return true;
+		}
+		return false;
+	}
+
+	openFileExplorer = async (
+		files: TFile | TFile[] | TFolder | TFolder[],
+		move?: boolean
+	) => {
+		const selectedPath: string = await this.picker();
+		if (!selectedPath) return;
+		let runModal;
+		if (Array.isArray(files)) {
+			for (const file of files) {
+				runModal = await this.doesFileExists(file, selectedPath);
+			}
+		} else {
+			runModal = await this.doesFileExists(files, selectedPath);
+		}
+		if (runModal) {
+			new OutFromVaultConfirmModal(this.app, this, (result) => {
+				console.debug("result", result);
+				if (result === 0) {
+					console.debug("closed");
+					return;
+				}
+				this.handler(result, files, selectedPath, move);
+			}).open();
+		} else {
+			if (move) await this.withoutModal(files, selectedPath, true);
+			else await this.withoutModal(files, selectedPath);
 		}
 	};
 
-	treatFiles(files: TFile | TFile[], selectedPath: string) {
-		if (Array.isArray(files) && files.length) {
-			for (const file of files)
-				this.MoveItem(file as TFile, selectedPath);
+	async withoutModal(
+		files: TFile | TFile[] | TFolder | TFolder[],
+		selectedPath: string,
+		move?: boolean
+	) {
+		if (Array.isArray(files)) {
+			for (const file of files) {
+				await this.simpleCopy(file, selectedPath, move);
+			}
 		} else {
-			this.MoveItem(files as TFile, selectedPath);
+			await this.simpleCopy(files, selectedPath, move);
+		}
+		new Notice(`File(s) copied to ${selectedPath}`, 4000);
+	}
+
+	async simpleCopy(
+		file: TFile | TFolder,
+		selectedPath: string,
+		move?: boolean
+	) {
+		const { normalizedFullPath, fileName, destinationPath } =
+			this.getDestinationPath(file as TFile | TFolder, selectedPath);
+		await fs.copy(normalizedFullPath, destinationPath);
+		if (move) {
+			this.app.vault.trash(file, true);
 		}
 	}
 
-	async MoveItem(file: TFile, selectedPath: string) {
-		if (!file) return;
+	handler(
+		result: number,
+		files: TFile | TFile[] | TFolder | TFolder[],
+		selectedPath: string,
+		move?: boolean
+	) {
+		if (Array.isArray(files)) {
+			for (const file of files)
+				this.MoveItem(
+					file as TFile | TFolder,
+					selectedPath,
+					result,
+					move
+				);
+		} else {
+			this.MoveItem(files as TFile | TFolder, selectedPath, result, move);
+		}
+		if (move) new Notice(`File(s) moved to ${selectedPath}`, 4000);
+		else new Notice(`File(s) copied to ${selectedPath}`, 4000);
+	}
+
+	async MoveItem(
+		file: TFile | TFolder,
+		selectedPath: string,
+		result: number,
+		move?: boolean
+	) {
 		const { normalizedFullPath, fileName, destinationPath } =
-			this.getDestinationPath(file as TFile, selectedPath);
+			this.getDestinationPath(file as TFile | TFolder, selectedPath);
 
-		await this.doCopy(
-			normalizedFullPath,
+		this.makeCopy(
+			file,
 			fileName,
+			selectedPath,
+			normalizedFullPath,
 			destinationPath,
-			selectedPath
+			result,
+			move
 		);
+	}
 
-		// Remove the original file
-		// await fs.remove(normalizedFullPath);
-		console.debug("Original file has been deleted.");
+	getDestinationPath(file: TFile | TFolder, selectedPath: string) {
+		const filePath = (this.app as any).vault.adapter.getFullPath(file.path);
+		const normalizedFullPath = normalizePath(filePath);
+		const fileName = path.basename(normalizedFullPath);
+		const destinationPath = path.join(selectedPath, fileName);
+		return { normalizedFullPath, fileName, destinationPath };
 	}
 
 	async picker() {
@@ -115,104 +213,53 @@ export default class Tools extends Plugin {
 		return dirPath[0];
 	}
 
-	getDestinationPath(file: TFile, selectedPath: string) {
-		const filePath = (this.app as any).vault.adapter.getFullPath(file.path);
-		const normalizedFullPath = normalizePath(filePath);
-		const fileName = path.basename(normalizedFullPath);
-		const destinationPath = path.join(selectedPath, fileName);
-		return { normalizedFullPath, fileName, destinationPath };
-	}
-
-	async doCopy(
-		normalizedFullPath: string,
-		fileName: string,
-		destinationPath: string,
-		selectedPath: string
-	) {
-		const fileExists = await fs.pathExists(destinationPath);
-		if (fileExists) {
-			// file already exists
-			if (!this.applyAll)
-				new ConfirmModal(this.app, this, (result) => {
-					if (result === 1) { //replace
-						this.confirm(
-							fileName,
-							selectedPath,
-							normalizedFullPath,
-							destinationPath,
-							1
-						);
-						if (this.files && this.files.length) {
-							this.MoveItem(
-								this.files.pop() as TFile,
-								selectedPath
-							);
-						}
-					} else if (result === 2) { // increment
-						this.confirm(
-							fileName,
-							selectedPath,
-							normalizedFullPath,
-							destinationPath,
-							2
-						);
-						if (this.files && this.files.length) {
-							this.MoveItem(
-								this.files.pop() as TFile,
-								selectedPath
-							);
-						}
-					} else {
-						return;
-					}
-				}).open();
-		} else {
-			await fs.copy(normalizedFullPath, destinationPath);
-			console.debug("File was successfully copied.");
-		}
-	}
-
-	async confirm(
+	async makeCopy(
+		file: TFile | TFolder,
 		fileName: string,
 		selectedPath: string,
 		normalizedFullPath: string,
 		destinationPath: string,
-		choice: number
+		choice: number,
+		move?: boolean
 	) {
 		if (choice === 2) {
-			// Create an incremented version
-			const baseFileName = path.parse(fileName).name;
-			const extension = path.parse(fileName).ext;
-			let version = 1;
-			let versionedFileName = `${baseFileName} (${version})${extension}`;
-			while (
-				await fs.pathExists(path.join(selectedPath, versionedFileName))
-			) {
-				version++;
-				versionedFileName = `${baseFileName} (${version})${extension}`;
+			console.log("choice 2");
+			const fileExists = await fs.pathExists(destinationPath);
+			if (fileExists) {
+				const baseFileName = path.parse(fileName).name;
+				const extension = path.parse(fileName).ext;
+				let version = 1;
+				let versionedFileName = `${baseFileName} (${version})${extension}`;
+				while (
+					await fs.pathExists(
+						path.join(selectedPath, versionedFileName)
+					)
+				) {
+					version++;
+					versionedFileName = `${baseFileName} (${version})${extension}`;
+				}
+				destinationPath = path.join(selectedPath, versionedFileName);
 			}
-			await fs.copy(
-				normalizedFullPath,
-				path.join(selectedPath, versionedFileName)
-			);
-			console.debug("File was successfully copied as", versionedFileName);
-		} else {
-			await fs.copy(normalizedFullPath, destinationPath);
-			console.debug("File was successfully moved.");
+			// Create an incremented version
 		}
+		await fs.copy(normalizedFullPath, destinationPath);
+		if (move) {
+			this.app.vault.trash(file, true);
+		}
+		console.debug("File was successfully copied as", destinationPath);
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
+	// async loadSettings() {
+	// 	this.settings = Object.assign(
+	// 		{},
+	// 		DEFAULT_SETTINGS,
+	// 		await this.loadData()
+	// 	);
+	// }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	// async saveSettings() {
+	// 	await this.saveData(this.settings);
+	// }
 }
 
 class ETVSettingTab extends PluginSettingTab {
